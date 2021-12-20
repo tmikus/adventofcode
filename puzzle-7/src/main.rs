1dc68cd8801215593e933a871760b8a2ab4cb6b2
Tomasz Mikus <tomasz@spiritai.com>
2021-12-20 10:47:42 +0000

    Add a complex solution to Puzzle 7

--- puzzle-7/src/main.rs
+++ puzzle-7/src/main.rs
@@ -2,21 +2,46 @@ use std::io;
 use std::io::prelude::*;
 use std::io::{Error, Lines, StdinLock};
 
-fn compute_fuel_expenditure(positions: Vec<u16>, median: u16) -> u32 {
+fn compute_complex_fuel_expenditure(positions: &Vec<u32>, ideal_position: u32) -> u32 {
     let mut fuel_used = 0;
     for position in positions {
-        fuel_used += (position as i32 - median as i32).abs() as u32;
+        let diff = (*position as i32 - ideal_position as i32).abs() as u32;
+        for num in 1..=diff {
+            fuel_used += num;
+        }
     }
     fuel_used
 }
 
-fn compute_median(positions: &mut Vec<u16>) -> u16 {
+fn compute_simple_fuel_expenditure(positions: Vec<u32>, ideal_position: u32) -> u32 {
+    let mut fuel_used = 0;
+    for position in positions {
+        fuel_used += (position as i32 - ideal_position as i32).abs() as u32;
+    }
+    fuel_used
+}
+
+fn compute_median(positions: &mut Vec<u32>) -> u32 {
     positions.sort();
     let middle = positions.len() / 2;
     positions[middle]
 }
 
-fn parse_positions(line: String) -> Vec<u16> {
+fn optimise_position(positions: Vec<u32>) -> u32 {
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+    let mut min_fuel_used = u32::MAX;
+    for position in min..=max {
+        println!("{}/{}", position, max);
+        let fuel_used = compute_complex_fuel_expenditure(&positions, position);
+        if fuel_used < min_fuel_used {
+            min_fuel_used = fuel_used;
+        }
+    }
+    min_fuel_used
+}
+
+fn parse_positions(line: String) -> Vec<u32> {
     line.split(',').map(|value| value.parse().unwrap()).collect()
 }
 
@@ -24,7 +49,8 @@ fn main() {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
     let mut positions = parse_positions(lines.next().unwrap().unwrap());
-    let median = compute_median(&mut positions);
-    let fuel_expenditure = compute_fuel_expenditure(positions, median);
+    // let ideal_position = compute_median(&mut positions); // Simple Solution
+    // let fuel_expenditure = compute_simple_fuel_expenditure(positions, ideal_position);
+    let fuel_expenditure = optimise_position(positions);
     println!("Result: {}", fuel_expenditure);
 }
