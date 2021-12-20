910ea6fc3338d5bfce80fa9ace8833d4eabaa919
Tomasz Mikus <tomasz@spiritai.com>
2021-12-20 20:54:02 +0000

    Add complex solution to puzzle 9

--- puzzle-9/src/main.rs
+++ puzzle-9/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::io;
 use std::io::prelude::*;
 use std::io::{Error, Lines, StdinLock};
@@ -21,12 +22,63 @@ impl Board {
         let mut result = 0;
         for y in 0..self.height {
             for x in 0..self.width {
-                if self.is_local_minimum(x as usize, y as usize) {
+                if self.is_local_minimum(x, y) {
                     result += 1 + self.get(x, y) as u32;
                 }
             }
         }
         result
+    }
+
+    fn compute_complex_result(&self) -> u64 {
+        let mut sizes = vec![];
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if self.is_local_minimum(x, y) {
+                    sizes.push(self.compute_basin_size(x, y));
+                }
+            }
+        }
+        sizes.sort();
+        println!("{:?}", sizes);
+        let len = sizes.len();
+        sizes[len - 1]
+          * sizes[len - 2]
+          * sizes[len - 3]
+    }
+
+    fn compute_basin_size(&self, start_x: usize, start_y: usize) -> u64 {
+        let mut visit_map = vec![false; self.data.len()];
+        let mut size = 0;
+        let mut point_queue = VecDeque::from([(start_x, start_y)]);
+        while !point_queue.is_empty() {
+            let (x, y) = point_queue.pop_front().unwrap();
+            if visit_map[y * self.width + x] {
+                continue;
+            }
+            visit_map[y * self.width + x] = true;
+            if self.get(x, y) == 9 {
+                continue;
+            }
+            size += 1;
+            // Try move left
+            if x > 0 && !visit_map[y * self.width + x - 1] {
+                point_queue.push_back((x - 1, y));
+            }
+            // Try move right
+            if x < (self.width - 1) && !visit_map[y * self.width + x + 1] {
+                point_queue.push_back((x + 1, y));
+            }
+            // Try move up
+            if y > 0 && !visit_map[(y - 1) * self.width + x] {
+                point_queue.push_back((x, y - 1));
+            }
+            // Try move down
+            if y < (self.height - 1) && !visit_map[(y + 1) * self.width + x] {
+                point_queue.push_back((x, y + 1));
+            }
+        }
+        size
     }
 
     fn get(&self, x: usize, y: usize) -> u8 {
@@ -82,5 +134,6 @@ impl Board {
 fn main() {
     let stdin = io::stdin();
     let board = Board::parse(stdin.lock().lines());
-    println!("Result: {}", board.count_local_minima());
+    // println!("Result: {}", board.count_local_minima()); // Simple Result
+    println!("Result: {}", board.compute_complex_result());
 }
